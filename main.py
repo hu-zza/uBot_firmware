383b5b7b000280aff779c8717345ed8fb8a58539
Szabó László András <48407681+hu-zza@users.noreply.github.com>
2020-12-25 23:40:04 +0100

    test server - full request

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-def web_page(path):
-  if led.value() == 1:
-    gpio_state="ON"
-  else:
-    gpio_state="OFF"
+def web_page(path, request):
   
-  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  html = """<html><head> <title>ESP Test Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
   h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
   border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
-  <p>GPIO state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p>
-  <p>Path: <strong>""" + path + """</strong></p></body></html>"""
+  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Test Server</h1> 
+  <p>Path: <strong>""" + path + """</strong></p>
+  <p>Request: <strong>""" + request + """</strong></p></body></html>"""
   return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,6 +19,7 @@ while True:
   request = conn.recv(1024)
   request = str(request)
   print('Content = %s' % request)
+
   led_on = request.find('/?led=on')
   led_off = request.find('/?led=off')
   if led_on == 6:
@@ -36,7 +32,7 @@ while True:
   path_end = request.find(' HTTP') 
   path = request[6:path_end]
   
-  response = web_page(path)
+  response = web_page(path, request)
   conn.send('HTTP/1.1 200 OK\n')
   conn.send('Content-Type: text/html\n')
   conn.send('Connection: close\n\n')
