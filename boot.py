8b6dc375e7cd38151d7a06b6288f108c9be7ff4d
Szabó László András <48407681+hu-zza@users.noreply.github.com>
2020-12-26 17:36:56 +0100

    AP settings,

--- boot.py
+++ boot.py
@@ -1,31 +1,22 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
-
 #import esp
 #esp.osdebug(None)
 
-from machine import Pin
-import uos, machine, gc, webrepl
+import uos, machine, gc, webrepl, network
 #uos.dupterm(None,1) # disable REPL on UART(0)
-led = Pin(2, Pin.OUT)
 gc.enable()
 webrepl.start()
+
+from machine import Pin
+led = Pin(2, Pin.OUT)
+led.value(1)
 
 try:
   import usocket as socket
 except:
   import socket
-  
-import network
 
-#ssid = ''
-#password = ''
-
-#station = network.WLAN(network.STA_IF)
-
-#station.active(True)
-#station.connect(ssid, password)
-
-#while station.isconnected() == False:
-#  pass
-
-
+ap = network.WLAN(network.AP_IF)
+ap.ifconfig(('192.168.11.1', '255.255.255.0', '192.168.11.1', '192.168.11.1'))
+ap.config(authmode = network.AUTH_WPA_WPA2_PSK)
+ap.config(essid = 'uBot_01')
+ap.config(password = 'uBot_01_pwd')
