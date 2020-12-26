158a8476e2d33ae11d7f2816db605956a3bc2aca
Szabó László András <48407681+hu-zza@users.noreply.github.com>
2020-12-26 16:06:45 +0100

    minimalistic, but proper request handling

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def getWebPage(method, path, body):
+def getWebPage(method, path, length, type, body):
+    if length < 1:
+        type = '-'
+        body = '-'
+
+    length = str(length)
+
     return """<html>
     <head><title>ESP Web Server</title></head>
     <body><table>
-    <tr><td>Method:&nbsp;</td><td>""" + method + """</td></tr>
-    <tr><td>Path:</td><td>""" + path + """</td></tr>
-    <tr><td>Body:</td><td>""" + body + """</td></tr>
+    <tr><td>Method: </td><td>""" + method + """</td></tr>
+    <tr><td>Path: </td><td>"""   + path   + """</td></tr>
+    <tr><td>Length: </td><td>""" + length + """</td></tr>
+    <tr><td>Type: </td><td>"""   + type   + """</td></tr>
+    <tr><td>Body: </td><td>"""   + body   + """</td></tr>
     </table></body>
     </html>"""
 
@@ -15,47 +23,49 @@ s.listen(5)
 while True:
     method = ''
     path = ''
-    contentLength = 0 #TODO..... 
+    contentLength = 0
+    contentType = ''
     body = ''
 
     connection, address = s.accept()
-    print('Got a connection from %s' % str(address))
+    requestFile         = connection.makefile('rwb', 0)
 
-    requestFile = connection.makefile('rwb', 0)
     while True:
         line = requestFile.readline()
+
         if not line:
             break
         elif line == b'\r\n':
-            if method == 'POST':
-                body = str(requestFile.readline(), 'utf-8')
-                print(body)
+            if 0 < contentLength:
+                body = str(requestFile.read(contentLength), 'utf-8')
             break
 
         line = str(line, 'utf-8')
-        print(line)
 
         if method == '':
             firstSpace = line.find(' ')
-            pathEnd = line.find(' HTTP')
+            pathEnd    = line.find(' HTTP')
 
             method = line[0:firstSpace]
-            path = line[firstSpace+1:pathEnd]
+            path   = line[firstSpace+1:pathEnd]
 
+            #until payload parsing
+            ledOn  = line.find(' /?led=on')
+            ledOff = line.find(' /?led=off')
 
-        ledOn = line.find(' /?led=on')
-        ledOff = line.find(' /?led=off')
+            if ledOn == firstSpace:
+                led.value(0)
+            elif ledOff == firstSpace:
+                led.value(1)
 
-        if ledOn == firstSpace:
-            print('LED ON')
-            led.value(0)
-        elif ledOff == firstSpace:
-            print('LED OFF')
-            led.value(1)
+        if 0 <= line.find('Content-Length:'):
+            contentLength = int(line[15:].strip())
 
-    response = getWebPage(method, path, body)
+        if 0 <= line.find('Content-Type:'):
+            contentType = line[13:].strip()
+
     connection.send('HTTP/1.1 200 OK\n')
     connection.send('Content-Type: text/html\n')
     connection.send('Connection: close\n\n')
-    connection.sendall(response)
+    connection.sendall(getWebPage(method, path, contentLength, contentType, body))
     connection.close()
